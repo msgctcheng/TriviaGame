f88655eeca90330626b5f1794a5479df0bea514f
rawrzombii <rawrzombii@rawrzombiisiMac.nc.rr.com>
2017-07-25 01:11:49 -0400

    Commit 1 - HTML, Assets, and Some Code Added.

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+//mainWrapper 
+
+//startWrapper (Start Page)
+
+//startButton
+
+//gameWrapper
+$(".startWrapper").hide();
+$(".boardWrapper").hide();
+//$(".ques12").hide();
+$(".ques13").hide();
+$(".ques14").hide();
